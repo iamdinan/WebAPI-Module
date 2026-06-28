e3f93d64895af42df06fbf8e72e94e215e131de6
Dinan <dinandilmith58@gmail.com>
2026-06-28 12:16:29 +0530

    S2: hello-world app

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "ok",
+    session: "NB6007CEM S2",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
